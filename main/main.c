965f944f78e95f4fbd537f2f6db4597a5a2af3ec
cjia-mac <cjia3254@gmail.com>
2025-08-31 00:24:29 +0800

    ota升级测试

--- main/main.c
+++ main/main.c
@@ -30,29 +30,29 @@ void app_main(void)
 
     spi_mutex = xSemaphoreCreateMutex();
 
-    ESP_LOGI(TAG, "Initializing LittleFS");
+    // ESP_LOGI(TAG, "Initializing LittleFS");
 
-    esp_vfs_littlefs_conf_t conf = {
-        .base_path = "/littlefs",
-        .partition_label = "storage",
-        .format_if_mount_failed = true,
-        .dont_mount = false,
-    };
+    // esp_vfs_littlefs_conf_t conf = {
+    //     .base_path = "/littlefs",
+    //     .partition_label = "storage",
+    //     .format_if_mount_failed = true,
+    //     .dont_mount = false,
+    // };
 
-    // Use settings defined above to initialize and mount LittleFS filesystem.
-    // Note: esp_vfs_littlefs_register is an all-in-one convenience function.
-    esp_err_t ret = esp_vfs_littlefs_register(&conf);
+    // // Use settings defined above to initialize and mount LittleFS filesystem.
+    // // Note: esp_vfs_littlefs_register is an all-in-one convenience function.
+    // esp_err_t ret = esp_vfs_littlefs_register(&conf);
 
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(TAG, "Failed to find LittleFS partition");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize LittleFS (%s)", esp_err_to_name(ret));
-        }
-        return;
-    }
+    // if (ret != ESP_OK) {
+    //     if (ret == ESP_FAIL) {
+    //         ESP_LOGE(TAG, "Failed to mount or format filesystem");
+    //     } else if (ret == ESP_ERR_NOT_FOUND) {
+    //         ESP_LOGE(TAG, "Failed to find LittleFS partition");
+    //     } else {
+    //         ESP_LOGE(TAG, "Failed to initialize LittleFS (%s)", esp_err_to_name(ret));
+    //     }
+    //     return;
+    // }
 
     printf("TEST ESP LVGL port\n\r");
 
